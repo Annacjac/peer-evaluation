087aa96c9d97cf9cd538d52fa72c71ac5a4bacc1
tannerhendrix <162663030+tannerhendrix@users.noreply.github.com>
2024-04-07 16:57:19 -0500

    Update SeniorDesignTeamController.java

--- peer-eval-backend/src/test/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeamController.java
+++ peer-eval-backend/src/test/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeamController.java
@@ -1,13 +1,31 @@
 @RestController
+@RequestMapping()
 public class SeniorDesignTeamController{
 
+    @GetMapping
     public Result findAllTeams{}
+    
+    @GetMapping
     public Result findByTeamName{}
+    
+    @GetMapping
     public Result findByStudent{}
+    
+    @GetMapping
     public Result findByInstructor{}
+    
+    @PostMapping
     public Result addTeam{}
+    
+    @DeleteMapping
     public Result deleteTeam{}
+    
+    @DeleteMapping
     public Result removeStudent{}
+    
+    @PostMapping
     public Result addStudent{}
+    
+    @GetMapping
     public Result findBySection{}
 }

8d9c9ba60f2c8baa7b99bfc54ebb226cc9f93e90
tannerhendrix <162663030+tannerhendrix@users.noreply.github.com>
2024-04-07 17:34:54 -0500

    Delete peer-eval-backend/src/test/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam directory

--- peer-eval-backend/src/test/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeamService.java
+++
@@ -1,9 +0,0 @@
-@Service
-@Transactional
-public class SeniorDesignTeamService{
-    public SeniorDesignTeam findByName{}
-    public SeniorDesignTeam findAllTeams{}
-    public SeniorDesignTeam save{}
-    public SeniorDesignTeam update{}
-    public void delete{}
-}

5fd71c26e3e04f0d95d7e5f06fa3791f7515f94a
Genesect01 <wapeck88@gmail.com>
2024-04-12 09:15:25 -0500

    Section Controller, Repository and Service added. Basic Framework for use case 1 added.

---
+++ peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/section/SectionService.java
@@ -0,0 +1,21 @@
+package edu.tcu.cs.peerevalbackend.section;
+
+import edu.tcu.cs.peerevalbackend.section.Section;
+//import edu.tcu.cs.peerevalbackend.repository.SectionRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+@Service
+public class SectionService {
+
+    @Autowired
+    private SectionRepository repository;
+
+    public List<Section> findSections(String sectionName, String academicYear) {
+        Sort sort = Sort.by(Sort.Order.desc("academicYear"), Sort.Order.asc("sectionName"));
+        return repository.findBySectionNameAndAcademicYear(sectionName, academicYear, sort);
+    }
+}
--- peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeam.java
+++ peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeam.java
@@ -1,17 +1,21 @@
-package seniorDesignTeam;
+package edu.tcu.cs.peerevalbackend.seniorDesignTeam;
 
 
+import edu.tcu.cs.peerevalbackend.instructor.Instructor;
+import edu.tcu.cs.peerevalbackend.section.Section;
+import edu.tcu.cs.peerevalbackend.student.Student;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
     public class SeniorDesignTeam implements Serializable {
         @Id
         private String name;
 
-        @OneToMany(mappedBy="name", CascadeType.PERSIST, CascadeType.MERGE)
+        @OneToMany(mappedBy="name", cascade={CascadeType.PERSIST, CascadeType.MERGE})
         private List<Student> students = new ArrayList<>();
 
         @OneToOne

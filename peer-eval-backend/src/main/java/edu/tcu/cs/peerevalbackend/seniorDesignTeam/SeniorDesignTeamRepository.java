a71bf86b250e45f0b654fee89792bc8aea22fb58
Tanner Hendrix <tannerhendrix@Tanners-MacBook-Pro.local>
2024-04-24 10:31:17 -0500

    Senior design team work

--- peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeamRepository.java
+++ peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeamRepository.java
@@ -1,8 +1,9 @@
-//package edu.tcu.cs.peerevalbackend.seniorDesignTeam;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public class SeniorDesignTeamRepository extends JpaRepository<SeniorDesignTeam, String> {
-//}
+package edu.tcu.cs.peerevalbackend.seniorDesignTeam;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeniorDesignTeamRepository extends JpaRepository<SeniorDesignTeam, String> {
+
+}
---
+++ peer-eval-backend/src/test/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeamControllerTest.java
@@ -0,0 +1,20 @@
+package edu.tcu.cs.peerevalbackend.seniorDesignTeam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeniorDesignTeamControllerTest {
+
+    @Test
+    void findByTeamName() {
+    }
+
+    @Test
+    void findByStudent() {
+    }
+
+    @Test
+    void removeStudent() {
+    }
+}

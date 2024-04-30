6e243bfef314fd1e0b004a5eb5a3c1f4e4713196
vishalseelam <158001075+vishalseelam@users.noreply.github.com>
2024-04-30 02:31:02 -0500

    Updated Instructor.java

--- peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/instructor/Instructor.java
+++ peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/instructor/Instructor.java
@@ -1,53 +1,87 @@
 package edu.tcu.cs.peerevalbackend.instructor;
 
+import edu.tcu.cs.peerevalbackend.section.Section;
 import edu.tcu.cs.peerevalbackend.seniorDesignTeam.SeniorDesignTeam;
 import jakarta.persistence.*;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-public class Instructor {
+public class Instructor implements Serializable {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO) //automatically generate Id starting from default 1
+    private Integer id;
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private boolean isActive;
+    private String name;
 
-    @OneToMany(mappedBy = "instructor")
-    private Set<SeniorDesignTeam> teams = new HashSet<>();
+//    private String password;
 
-    public Instructor() {}
+    //when one instructor is saved, all sections for that instructor is saved as well.
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "owner")
+    private List<Section> sections = new ArrayList<>();
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "instructor")
+    private List<SeniorDesignTeam> teams = new ArrayList<>();
 
-    public Long getId() {
+    public Instructor() {
+    }
+
+    public Integer getId() {
         return id;
     }
 
-
-    public void setId(Long id) {
+    public void setId(int id) {
         this.id = id;
     }
-    public String getFirstName() {
-        return firstName;
-    }
-    //get last name
-    public String getLastName() {
-        return lastName;
-    }
-    public String getEmail() {
-        return email;
+
+    public String getName() {
+        return name;
     }
 
-    public boolean isActive() {
-        return isActive;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setActive(boolean active) {
-        isActive = active;
+    public List<Section> getSections() {
+        return sections;
     }
 
-    // Getters and Setters
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
+    }
+
+//    public void addSection(Section section) {
+//        section.setAdmin(this);
+//        this.sections.add(section);
+//    }
+
+    public Integer getNumberOfSections() {
+        return this.sections.size();
+    }
+
+    public void removeAllSections() {
+        this.sections.stream().forEach(section -> section.setAdmin(null));
+        this.sections = null;
+    }
+
+    public void removeSection(Section sectionToBeAssigned) {
+        //remove section owner
+        sectionToBeAssigned.setAdmin(null);
+        this.sections.remove(sectionToBeAssigned);
+    }
+
+    public List<SeniorDesignTeam> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<SeniorDesignTeam> teams) {
+        this.teams = teams;
+    }
+
+    public void addTeam(SeniorDesignTeam seniorDesignTeam) {
+        seniorDesignTeam.getInstructors().add(this);
+        this.teams.add(seniorDesignTeam);
+    }
 }

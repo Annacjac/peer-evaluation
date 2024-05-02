3127a513a9263cded1433b7317f14f2d9c1fe487
Tanner Hendrix <tannerhendrix@Tanners-MBP.attlocal.net>
2024-05-02 16:33:20 -0500

    fixing student list

--- peer-eval-frontend/src/student/StudentList.vue
+++ peer-eval-frontend/src/student/StudentList.vue
@@ -1,8 +1,7 @@
 <template>
-    <el-button style='margin: 20px'>Add Student</el-button>
-    <el-button style='margin: 20px'>Generate WAR Report</el-button>
+    <el-button style='margin-bottom: 20px'>Add Student</el-button>
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column fixed prop="id" label="ID" width="150" />
+      <el-table-column fixed prop="id" label="ID" width="50" />
       <el-table-column prop="name" label="Name" width="120" />
       <el-table-column prop="year" label="Year" width="120" />
       <el-table-column prop="teamName" label="Team Name" width="120" />
@@ -11,6 +10,11 @@
       <el-table-column prop="generatePeerEvaluation" label="Generate Peer Evaluation" width="200" >
         <template #default>
           <el-button link type="primary" size="small" @click="handleClick">Generate Peer Evaluation</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column prop="generateWarReport" label="Generate WAR Report" width="200">
+        <template #default>
+          <el-button link type="primary" size="small" @click="handleClick">Generate WAR Report</el-button>
         </template>
       </el-table-column>
 
@@ -39,6 +43,7 @@
       sectionName: 'Section-1',
       email: 'tomldoe@gmail.com',
       generatePeerEvaluation: 'Generate Peer Evaluation',
+      generateWarReport:'Generate WAR Report',
       tag: 'Home',
     },
     {
@@ -49,6 +54,7 @@
       sectionName: 'Section-1',
       email: 'tomsdoe@gmail.com',
       generatePeerEvaluation: 'Generate Peer Evaluation',
+      generateWarReport:'Generate WAR Report',
       tag: 'Office',
     },
     {
@@ -59,6 +65,7 @@
       sectionName: 'Section-1',
       email: 'tommdoe@gmail.com',
       generatePeerEvaluation: 'Generate Peer Evaluation',
+      generateWarReport:'Generate WAR Report',
       tag: 'Home',
     },
     {
@@ -69,6 +76,7 @@
       sectionName: 'Section-1',
       email: 'tomndoe@gmail.com',
       generatePeerEvaluation: 'Generate Peer Evaluation',
+      generateWarReport:'Generate WAR Report',
       tag: 'Home',
     },
   ]

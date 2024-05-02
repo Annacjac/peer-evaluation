298e8b0ece7b0b12a4ac995d603918e4ddbef25d
Tanner Hendrix <tannerhendrix@Tanners-MBP.attlocal.net>
2024-05-02 16:16:54 -0500

    fixing student list

--- peer-eval-frontend/src/student/StudentList.vue
+++ peer-eval-frontend/src/student/StudentList.vue
@@ -3,12 +3,13 @@
     <el-button style='margin: 20px'>Generate Peer Evaluation</el-button>
     <el-button style='margin: 20px'>Generate WAR Report</el-button>
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column fixed prop="date" label="Date" width="150" />
+      <el-table-column fixed prop="id" label="ID" width="150" />
       <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="600" />
-      <el-table-column prop="zip" label="Zip" width="120" />
+      <el-table-column prop="year" label="Year" width="120" />
+      <el-table-column prop="teamName" label="Team Name" width="120" />
+      <el-table-column prop="sectionName" label="Section Name" width="120" />
+      <el-table-column prop="email" label="Email" width="300" />
+      <el-table-column prop="generatePeerEvaluation" label="Generate Peer Evaluation" width="120" />
       <el-table-column fixed="right" label="Operations" width="120">
         <template #default>
           <el-button link type="primary" size="small" @click="handleClick">
@@ -27,40 +28,44 @@
   
   const tableData = [
     {
-      date: '2016-05-03',
-      name: 'Tom',
-      state: 'California',
-      city: 'Los Angeles',
-      address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
+      id: '1',
+      name: 'Tom L. Doe',
+      year: 'Senior',
+      teamName: 'Team-1',
+      sectionName: 'Section-1',
+      email: 'tomldoe@gmail.com',
+      generatePeerEvaluation: 'Generate Peer Evaluation',
       tag: 'Home',
     },
     {
-      date: '2016-05-02',
-      name: 'Tom',
-      state: 'California',
-      city: 'Los Angeles',
-      address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
+      id: '2',
+      name: 'Tom S. Doe',
+      year: 'Senior',
+      teamName: 'Team-1',
+      sectionName: 'Section-1',
+      email: 'tomsdoe@gmail.com',
+      generatePeerEvaluation: 'Generate Peer Evaluation',
       tag: 'Office',
     },
     {
-      date: '2016-05-04',
-      name: 'Tom',
-      state: 'California',
-      city: 'Los Angeles',
-      address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
+      id: '3',
+      name: 'Tom M. Doe',
+      year: 'Senior',
+      teamName: 'Team-1',
+      sectionName: 'Section-1',
+      email: 'tommdoe@gmail.com',
+      generatePeerEvaluation: 'Generate Peer Evaluation',
       tag: 'Home',
     },
     {
-      date: '2016-05-01',
-      name: 'Tom',
-      state: 'California',
-      city: 'Los Angeles',
-      address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
-      tag: 'Office',
+      id: '4',
+      name: 'Tom N. Doe',
+      year: 'Senior',
+      teamName: 'Team-1',
+      sectionName: 'Section-1',
+      email: 'tomndoe@gmail.com',
+      generatePeerEvaluation: 'Generate Peer Evaluation',
+      tag: 'Home',
     },
   ]
   </script>

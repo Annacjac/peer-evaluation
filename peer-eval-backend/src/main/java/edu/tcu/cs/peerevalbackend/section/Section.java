b261d804eff6be4b63d34e035d09122aa3a72524
vishalseelam <158001075+vishalseelam@users.noreply.github.com>
2024-05-03 11:01:02 -0500

    DBDataInitializer created

--- peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/section/Section.java
+++ peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/section/Section.java
@@ -2,6 +2,7 @@ package edu.tcu.cs.peerevalbackend.section;
 
 import edu.tcu.cs.peerevalbackend.admin.Admin;
 import edu.tcu.cs.peerevalbackend.rubric.Rubric;
+import edu.tcu.cs.peerevalbackend.seniorDesignTeam.SeniorDesignTeam;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -12,10 +13,10 @@ import java.util.List;
 public class Section implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private String id; //Section name is ID
 
-    //@OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "section")
-    //private List<SeniorDesignTeam> team = new ArrayList<>();
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "section")
+    private List<SeniorDesignTeam> team = new ArrayList<>();
 
     private String academicYear;
 
@@ -24,19 +25,23 @@ public class Section implements Serializable {
 
     @ManyToOne
     private Admin admin;
+    private String firstDate;
+
+    private String lastDate;
 
     @ManyToOne
     private Rubric rubric;
-
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, mappedBy = "section")
+    private List<SeniorDesignTeam> teams  =  new ArrayList<>();
     @ElementCollection
     private List<String> activeWeeks =  new ArrayList<>();
 
     // Getters and Setters
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -54,6 +59,21 @@ public class Section implements Serializable {
 
     public String getAcademicYear(){
         return academicYear;
+    }
+    public String getFirstDate() {
+        return firstDate;
+    }
+
+    public void setFirstDate(String firstDate) {
+        this.firstDate = firstDate;
+    }
+
+    public String getLastDate() {
+        return lastDate;
+    }
+
+    public void setLastDate(String lastDate) {
+        this.lastDate = lastDate;
     }
     public Admin getAdmin() {
         return admin;

16c1f678170e713a50593fc2dab871c60c1d3e12
vishalseelam <158001075+vishalseelam@users.noreply.github.com>
2024-05-03 09:09:17 -0500

    Worked on UC-9

--- peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeam.java
+++ peer-eval-backend/src/main/java/edu/tcu/cs/peerevalbackend/seniorDesignTeam/SeniorDesignTeam.java
@@ -1,6 +1,7 @@
 package edu.tcu.cs.peerevalbackend.seniorDesignTeam;
 
 
+import edu.tcu.cs.peerevalbackend.admin.Admin;
 import edu.tcu.cs.peerevalbackend.instructor.Instructor;
 import edu.tcu.cs.peerevalbackend.section.Section;
 import edu.tcu.cs.peerevalbackend.student.Student;
@@ -25,6 +26,8 @@ import java.util.List;
 
     @ManyToOne
     private Section section;
+    @ManyToOne
+    private Admin admin;
 
     public SeniorDesignTeam() {
     }
@@ -73,5 +76,8 @@ import java.util.List;
 
     public void setStudents(List<Student> students) {
         this.students = students;
+    }
+
+    public void setAdmin(Admin admin) { this.admin = admin;
     }
 }
